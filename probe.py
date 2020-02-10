80f6885d5bc3ea3ed323936364ad01b72c00def0
“Liubov <kvi007hp@gmail.com>
2020-02-10 21:05:27 +0200

    probe file has been updated with message

--- probe.py
+++ probe.py
@@ -1,7 +1,10 @@
-ccc=[10, 4, 5, 2]
-for a in ccc:
-    if a % 2 != 0:
-        break
-    else:
-        print("List contains even values only")
-print("List contains odd values, such as", a)
+# ccc=[10, 4, 5, 2]
+# for a in ccc:
+#     if a % 2 != 0:
+#         break
+#     else:
+#         print("List contains even values only")
+# print("List contains odd values, such as", a)
+
+
+print ("""I need help with this""")
